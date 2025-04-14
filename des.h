48f2e83812b939729b2b45ff9fbaefed835c7f64
NguyenBach-bk <148066809+NguyenBach-bk@users.noreply.github.com>
2025-04-15 01:07:04 +0700

    Update des.h

--- des.h
+++ des.h
@@ -1,9 +1,33 @@
 #ifndef DES_H
 #define DES_H
 
-#include <string>
+#include <cstdint>
+#include <vector>
 
-void des_encrypt(const std::string &input, const std::string &key, std::string &output);
-void des_decrypt(const std::string &input, const std::string &key, std::string &output);
+class DES
+{
+public:
+  DES (const std::vector<uint8_t> &key);
+
+  std::vector<uint8_t> encrypt (const std::vector<uint8_t> &plaintext);
+  std::vector<uint8_t> decrypt (const std::vector<uint8_t> &ciphertext);
+
+private:
+  void generateSubKeys ();
+  uint64_t desProcessBlock (uint64_t block, bool encrypt);
+
+  std::vector<uint8_t> key;
+  std::vector<uint64_t> subKeys;
+
+  // Table and S-box
+  static const int initialPermutation[64];
+  static const int finalPermutation[64];
+  static const int expansionPermutation[48];
+  static const int permutation[32];
+  static const int pc1[56];
+  static const int pc2[48];
+  static const int shiftTable[16];
+  static const int sBox[8][4][16];
+};
 
 #endif // DES_H
